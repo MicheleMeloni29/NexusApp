1d80a8b51cb74359fe2729c7d13ad487f4dee224
MicheleMeloni29 <melonimichelee29@gmail.com>
2026-01-27 17:28:03 +0100

    Try fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { LanguageProvider } from "@/app/components/LanguageProvider";
 
@@ -11,6 +12,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const vermillion22 = localFont({
+  src: "./fonts/Vermillion22.otf",
+  variable: "--font-vermillion-22",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${vermillion22.variable} antialiased`}
       >
         <LanguageProvider>{children}</LanguageProvider>
       </body>
